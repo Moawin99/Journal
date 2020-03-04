f700374126af0d03ed2480f4339a5c347851048b
Mark Moawad <msmoawad99@gmail.com>
2020-03-03 22:39:58 -0500

    Basic Entry test

---
+++ input.js
@@ -0,0 +1,9 @@
+
+function print(){
+    var title = document.getElementById("entryTitle").value;
+    var mood = document.getElementById("moodDropDown").value;
+    var entry = document.getElementById("entry").value;
+    console.log(title);
+    console.log(mood);
+    console.log(entry);
+}
