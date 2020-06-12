e0aa107b710955ac83720f387ae099378012bf55
Mark Moawad <msmoawad99@gmail.com>
2020-06-12 11:56:32 -0400

    setting up dependices

--- input.js
+++
@@ -1,9 +0,0 @@
-
-function print(){
-    var title = document.getElementById("entryTitle").value;
-    var mood = document.getElementById("moodDropDown").value;
-    var entry = document.getElementById("entry").value;
-    console.log(title);
-    console.log(mood);
-    console.log(entry);
-}
