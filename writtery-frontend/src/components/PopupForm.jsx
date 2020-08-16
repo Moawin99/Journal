50c0cddc763e877ce42094ce15c212bba5f9520d
Moawin99 <msmoawad99@gmail.com>
2020-08-16 17:53:40 -0400

    testing ssh again

--- writtery-frontend/src/components/PopupForm.jsx
+++ writtery-frontend/src/components/PopupForm.jsx
@@ -1,1 +1,17 @@
 import React from 'react';
+
+const PopupForm = (props) => {
+	if (props.text === 'login') {
+		return (
+			<div className="popup-container">
+				<h2 className="popup-text">Login</h2>
+			</div>
+		);
+	} else {
+		return (
+			<div className="popup-container">
+				<h2 className="popup-text">Register</h2>
+			</div>
+		);
+	}
+};
