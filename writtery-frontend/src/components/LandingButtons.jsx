fbf48deb796c10a5a51f2de906cb8777e14402e9
Moawin99 <msmoawad99@gmail.com>
2020-08-16 21:21:33 -0400

    in the process of connecting frontend to backend

--- writtery-frontend/src/components/LandingButtons.jsx
+++ writtery-frontend/src/components/LandingButtons.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import '../stylesheets/landingButton.css';
+import PopupForm from './PopupForm';
 
 const Button = (props) => {
 	return (
-		<button className="button" id={props.id}>
+		<button className="button" id={props.id} onClick={() => <PopupForm text="login" />}>
 			<h2>{props.text}</h2>
 		</button>
 	);
--- writtery-frontend/src/components/PopupForm.jsx
+++ writtery-frontend/src/components/PopupForm.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 
 const PopupForm = (props) => {
+	const [ firstName, setFirstName ] = useState('');
+	const [ lastName, setLastName ] = useState('');
+	const [ username, setUsername ] = useState('');
+	const [ password, setPassword ] = useState('');
 	if (props.text === 'login') {
 		return (
 			<div className="popup-container">
 				<h2 className="popup-text">Login</h2>
+				<form>
+					<label>Username</label>
+					<input type="text" onChange={(e) => setUsername(e.target.value)} />
+					<label>Password</label>
+					<input type="text" onChange={(e) => setPassword(e.target.value)} />
+					<input
+						type="submit"
+						value="Login"
+						onClick={() =>
+							axios
+								.post('/login', {
+									username: username,
+									password: password
+								})
+								.then((res) => {
+									console.log(res);
+								})
+								.catch((error) => {
+									console.log(error);
+								})}
+					/>
+				</form>
 			</div>
 		);
 	} else {
@@ -15,3 +43,5 @@ const PopupForm = (props) => {
 		);
 	}
 };
+
+export default PopupForm;
