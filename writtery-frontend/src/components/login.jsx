01a06c9d199efc6191d5173b0b281c0120eac10d
Mark Moawad <msmoawad99@gmail.com>
2021-09-08 23:37:42 -0400

    adding css to login page

--- writtery-frontend/src/components/login.jsx
+++ writtery-frontend/src/components/login.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-
+import "../stylesheets/login.css";
 
 const Login = () => {
+	return(
 	<div className="page-container"> 
-		<div className="login-container">
+		<div className="login-box">
 			<img lang="temp pic" />
 			<div className="login-info">
 				<h2>User Login</h2>
 			</div>
 		</div>
 	</div>
-};
+	)};
 
 export default Login;
