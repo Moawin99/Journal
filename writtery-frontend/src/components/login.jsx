336af623c0acd43ce9e6e53091cbb59e4fe7754d
Mark Moawad <msmoawad99@gmail.com>
2021-09-12 23:35:05 -0400

    almost done with login page

--- writtery-frontend/src/components/login.jsx
+++ writtery-frontend/src/components/login.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import "../stylesheets/login.css";
+import loginPic from '../pictures/loginPic.svg';
 
 const Login = () => {
 	return(
 	<div className="page-container"> 
 		<div className="login-box">
-			<img lang="temp pic" />
+			<img src={loginPic} className="loginPic"/>
 			<div className="login-info">
 				<h2>User Login</h2>
 			</div>
