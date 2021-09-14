cf0f9ec54e8ad42fe29ea2a82f42c72e6e64af61
Mark Moawad <msmoawad99@gmail.com>
2021-09-13 21:40:44 -0400

    almost done with login css

--- writtery-frontend/src/components/login.jsx
+++ writtery-frontend/src/components/login.jsx
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../stylesheets/login.css";
 import loginPic from '../pictures/loginPic.svg';
+import emailPic from '../icons/email.png';
 
 const Login = () => {
+	const [email, setEmail] = useState();
+	const [password, setPassword] = useState();
 	return(
 	<div className="page-container"> 
 		<div className="login-box">
 			<img src={loginPic} className="loginPic"/>
 			<div className="login-info">
 				<h2>User Login</h2>
+				<div className="input-container">
+					<input className="input-field email" onChange={(e) => setEmail(e.target.value)} type="text" />
+					<input className="input-field password" onChange={(e) => setPassword(e.target.value)} type="password" />
+				</div>
+				<button className="loginBtn">Login</button>
+				<p>Create an Account</p> {/**To be wrapped with route tag to take to register page */}
 			</div>
 		</div>
 	</div>
