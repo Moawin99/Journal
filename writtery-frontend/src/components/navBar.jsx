54048e120d6b64396975ab873ae88e6679735798
Mark Moawad <msmoawad99@gmail.com>
2021-12-18 22:35:01 -0500

    started navbar

---
+++ writtery-frontend/src/components/navBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useState } from 'react';
+import Whitelogo from '../pictures/WhiteWritteryLogo.png';
+import Blacklogo from '../pictures/WritteryLogo.png';
+
+const Navbar = (props) => {
+	const [logo, setRightLogo] = useState();
+	const [userInfo, setUserinfo] = useState();
+	
+	return (
+		<nav>
+			
+		</nav>
+	)
+};
+
+
+export default NavBar;
