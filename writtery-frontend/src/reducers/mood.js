2183838b987042ae6642d1c927d79b3887d77b96
Mark Moawad <msmoawad99@gmail.com>
2021-12-27 20:27:20 -0500

    redux boilerplate

--- writtery-frontend/src/reducers/mood.js
+++ writtery-frontend/src/reducers/mood.js
@@ -1,21 +1,11 @@
-//NEED TO RETHINK HOW IMMA DO THE STATES
 
-// const moodReducer = (state = 'Happy', action) => { 
-// 	swtich(action.type){
-// 		case 'Happy':
-// 			return action.type
-// 		case 'Sad':
-// 			return action.type
-// 		case 'Confused':
-// 			return action.type
-// 		case 'Angry':
-// 			return action.type
-// 		case 'Relaxed':
-// 			return action.type
-// 		default:
-// 			return state;
+const moodReducer = (state = 'Happy', action) => {
+	switch(action.type){
+		case 'moodSelection':
+			return action.payload;
+		default:
+			return state;
+	}
+};
 
-// 	}
-// };
-
-// export default moodReducer;
+export default moodReducer;
