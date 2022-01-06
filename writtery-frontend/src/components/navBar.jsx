a5ce17f4b0b9a4a43995fde52f4846282af0f49d
Mark Moawad <msmoawad99@gmail.com>
2022-01-06 17:06:06 -0500

    added color styles to nav but mood state isn't being held across compoments

--- writtery-frontend/src/components/navBar.jsx
+++ writtery-frontend/src/components/navBar.jsx
@@ -4,13 +4,18 @@ import Whitelogo from '../pictures/WhiteWritteryLogo.png';
 import Blacklogo from '../pictures/WritteryLogo.png';
 import Hamburger from './Hamburger';
 import '../stylesheets/navBar.css';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 const Navbar = (props) => {
 	const [logo, setRightLogo] = useState();
 	const [userInfo, setUserinfo] = useState();
+	const mood = useSelector((state) => state.mood.value);
+	const dispatch = useDispatch();
+	let navStyle = "navContainer " + "nav" + mood;
 	
 	return (
-		<nav className="navMood navContainer">
+		<nav className={navStyle}>
 			<Hamburger />
 			<img className="logo" src={Whitelogo} />
 			<h1>Welcome User</h1>
