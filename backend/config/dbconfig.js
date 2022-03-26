816cba059ebf648b3d59eb6600e6fc2bd0f93318
Mark <msmoawad99@gmail.com>
2022-03-26 17:34:34 -0400

    rm db config bc different between systems

--- backend/config/dbconfig.js
+++ backend/config/dbconfig.js
@@ -1,13 +1,16 @@
-require('dotenv').config();
-const Pool = require('pg').Pool;
+require("dotenv").config();
+const Pool = require("pg").Pool;
 
 const pool = new Pool({
-	user: process.env.DB_USER,
-	host: process.env.HOST,
-	database: process.env.DB,
-	password: process.env.PASSWORD,
-	port: process.env.PORT
+  user: process.env.DB_USER,
+  host: process.env.REMOTE_HOST,
+  database: process.env.DB,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
 });
 
+pool.on("connect", () => {
+  console.log("Connected!");
+});
 
 module.exports = { pool };
