68d90978a133ce59a2597d27a0fd98087310b3e1
Mark <msmoawad99@gmail.com>
2022-11-03 13:56:17 -0400

    need to fix sizing and get imports to work

--- frontend/src/components/landing/LandingPage.jsx
+++ frontend/src/components/landing/LandingPage.jsx
@@ -1,19 +1,25 @@
-import { Box, Flex, Image } from '@chakra-ui/react';
 import React from 'react';
 import { getColors } from '../../utils/getColors'
 import logo from '../../assets/icons/WritteryLogo.png'
 import * as book_girl_pictures from '../../assets/pictures'
+import { 
+    Flex, 
+    Image, 
+    Button, 
+    Heading, 
+    Text } from '@chakra-ui/react';
 
 const LandingPage = ({ color }) => {
     const colors = getColors(color)
 
     return (
         <Flex
-        flexDir='row'
+        flexDir='column'
         h='100vh'
         w='100vw'
         bg={colors.primary}
         position={'relative'}
+        color={colors.text}
         >
             <Flex
             p='2em'
@@ -24,13 +30,54 @@ const LandingPage = ({ color }) => {
                 <Image src={logo} alt='Logo' h={'4em'}
                  />
                 <Flex
-                w={'25%'}
+                w={'10rem'}
                 justify={'space-between'}
                 h={'min'}
                 >
                     <p href='#about'>About</p>
                     <p href='#contact'>Contact</p>
                 </Flex>
+            </Flex>
+
+            <Flex
+            w='100%'
+            h='80%'
+            justify='flex-end'
+            align='center'
+            >
+
+                <Flex
+                w='50%'
+                justify='center'
+                align='center'
+                >
+                    <Flex
+                    className='slogan-container'
+                    h='15rem'
+                    w='80%'
+                    flexDir='column'
+                    justify='center'
+                    align='flex-start'
+                    >
+                        <Heading noOfLines={2} size='3xl'>Express Your Emotions</Heading>
+                        <Text fontSize='2xl'>Your own personal journal that matches your mood</Text>
+                        <Flex
+                        w='70%'
+                        h='4rem'
+                        justify='space-between'
+                        align='center'
+                        >
+                            <Button bg={colors.secondary} w='45%' color={'black'}>
+                                Join Now
+                            </Button>
+                            <Button bg={colors.btnColor} w='45%' color={colors.btnText}>
+                                Login
+                            </Button>
+
+                        </Flex>
+                    </Flex>
+                </Flex>
+
             </Flex>
 
 
